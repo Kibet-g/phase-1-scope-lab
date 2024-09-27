e3e92f0d9a714a89971e25bec9f17ca4635c076d
+ <kibetg984@gmail.com>
2024-09-27 10:20:25 +0300

    Added changes by creating global variables on our code

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+//we have declared the customer name in the global scope
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+  
+  function setBestCustomer() {
+    bestCustomer = 'not bob'; 
+  }
+
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+
+  const leastFavoriteCustomer = 'some customer'; 
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'another customer'; 
+}
+
